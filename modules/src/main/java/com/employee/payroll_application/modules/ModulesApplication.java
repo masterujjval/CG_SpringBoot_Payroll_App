a1f5cd5b331c57c2f43c072bb67f09796dcb3c28
masterujjval <guptarickey3@gmail.com>
2025-02-24 12:43:36 +0530

    [Ujjwal Gupta] ADD: main branch code added

---
+++ modules/src/main/java/com/employee/payroll_application/modules/ModulesApplication.java
@@ -0,0 +1,13 @@
+package com.employee.payroll_application.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulesApplication.class, args);
+	}
+
+}
